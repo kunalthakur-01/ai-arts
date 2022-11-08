b863f3cec7f85188096450c1ef6d269591773aef
kunal <13kunalthakur@gmail.com>
2022-11-08 12:15:21 +0530

    adding profile page

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Profile.module.css';
+
+const Profile = () => {
+  return (
+    <section className={styles.profile_section}>
+      
+    </section>
+  )
+}
+
+export default Profile
